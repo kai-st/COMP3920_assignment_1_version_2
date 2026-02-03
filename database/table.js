69a01867f39bab71a934dec68170b2655d80ea28
Kai Steingarten <ksteingarten@my.bcit.ca>
2026-02-02 18:55:17 -0800

    Allow longer usernames

--- database/table.js
+++ database/table.js
@@ -1,47 +1,45 @@
-const database = require('../databaseConnection');
+const database = require("../databaseConnection");
 
 async function createUserTable() {
-	let createUserSQL = `
+  let createUserSQL = `
 		CREATE TABLE IF NOT EXISTS user (
             user_id INT NOT NULL AUTO_INCREMENT,
-            username VARCHAR(25) NOT NULL,
+            username VARCHAR(500) NOT NULL,
             password VARCHAR(100) NOT NULL,
             PRIMARY KEY (user_id),
             UNIQUE INDEX unique_username (username ASC) VISIBLE);
 	`;
-	
-	try {
-		const results = await database.query(createUserSQL);
 
-        console.log("Successfully created user table");
-		console.log(results[0]);
-	}
-	catch(err) {
-		console.error("Error creating user table");
-        console.error(err);
-	}
+  try {
+    const results = await database.query(createUserSQL);
+
+    console.log("Successfully created user table");
+    console.log(results[0]);
+  } catch (err) {
+    console.error("Error creating user table");
+    console.error(err);
+  }
 }
 
 async function clearUserTable() {
-		let deleteAllUserSQL = `
+  let deleteAllUserSQL = `
 		DELETE FROM user;
 	`;
-	
-	try {
-		const results = await database.query(deleteAllUserSQL);
 
-        console.log("Successfully cleared user table");
-		console.log(results[0]);
-	}
-	catch(err) {
-		console.error("Error clearing user table");
-        console.error(err);
-	}
+  try {
+    const results = await database.query(deleteAllUserSQL);
+
+    console.log("Successfully cleared user table");
+    console.log(results[0]);
+  } catch (err) {
+    console.error("Error clearing user table");
+    console.error(err);
+  }
 }
 
 async function reset_database() {
-	await createUserTable();
-	await clearUserTable();
+  await createUserTable();
+  await clearUserTable();
 }
 
-module.exports = {createUserTable, clearUserTable, reset_database};
+module.exports = { createUserTable, clearUserTable, reset_database };
